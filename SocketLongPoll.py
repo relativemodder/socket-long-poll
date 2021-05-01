c8e1e4ab4dcdbfa92e9f5c4b357ed2f8d0c58599
Andrey <61388196+relativemodder@users.noreply.github.com>
2021-05-01 08:51:40 +0300

    Update SocketLongPoll.py

--- SocketLongPoll.py
+++ SocketLongPoll.py
@@ -8,25 +8,25 @@ tс = 0
 
 
 while True:
-    sock.listen(1)
+    sock.listen(1) # Listening for connection
     conn, addr = sock.accept()
     while True:
-        data = conn.recv(1024)
+        data = conn.recv(1024) # Receivin data
         if not data:
             break
         while True:
             try:
-                t = os.path.getmtime("pollFile.txt")
+                t = os.path.getmtime("pollFile.txt") # Gettin modification time
             except:
                 time.sleep(0.2)
                 t = os.path.getmtime("pollFile.txt")
             if(t>tс):
                 tс=t
                 time.sleep(0.2)
-                f = open("pollFile.txt", "r")
-                content = f.read(99999999)
+                f = open("pollFile.txt", "r") 
+                content = f.read(99999999) # Gettin file contents
                 f.close()
-                conn.send(("changed file to "+content).encode("utf-8"))
+                conn.send(("changed file to "+content).encode("utf-8")) # Returnin data to client
                 break
         
     conn.close()
